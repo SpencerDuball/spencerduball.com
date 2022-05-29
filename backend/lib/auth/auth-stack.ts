f1d8cc6f3bf735a4749245e317d2c5a973fd8b0c
Spencer Duball <spencer@spencerduball.com>
2022-05-29 15:34:53 -0500

    Auth flow complete, need to refactor and setup client side auth

--- backend/lib/auth/auth-stack.ts
+++ backend/lib/auth/auth-stack.ts
@@ -1,4 +1,9 @@
-import { Stack, StackProps, RemovalPolicy } from "aws-cdk-lib";
+import { Stack, StackProps, RemovalPolicy, Duration } from "aws-cdk-lib";
+import {
+  AwsCustomResource,
+  PhysicalResourceId,
+  AwsCustomResourcePolicy,
+} from "aws-cdk-lib/custom-resources";
 import { Construct } from "constructs";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import {
@@ -11,6 +16,11 @@ import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-al
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Table, AttributeType, BillingMode } from "aws-cdk-lib/aws-dynamodb";
+import {
+  UserPool,
+  AccountRecovery,
+  ClientAttributes,
+} from "aws-cdk-lib/aws-cognito";
 import path from "path";
 
 export class AuthStack extends Stack {
@@ -37,14 +47,91 @@ export class AuthStack extends Stack {
       },
     });
 
-    // create oauth CSRF validation table
-    const csrfTable = new Table(this, "GithubAuthCSRFTable", {
-      partitionKey: { name: "PK", type: AttributeType.STRING },
-      timeToLiveAttribute: "TTL",
-      removalPolicy: RemovalPolicy.DESTROY,
-      billingMode: BillingMode.PAY_PER_REQUEST,
+    /////////////////////////////////////////////////////////////////////////////
+    // (1) Custom User Pool
+    //
+    // With the custom user pool, we will allow our users to get an access token
+    // from github and then store the necessary user info in DynamoDB. This is
+    // necessary as with the Github credential alone, we cannot block or assign
+    // users to special groups. Special groups would allow expanded or blocked
+    // access to APIs.
+    //
+    // This is how we will actually manage the users in our apps, but we will use
+    // github to sign in.
+    /////////////////////////////////////////////////////////////////////////////
+    // create the user pool and client
+    const pool = new UserPool(this, "UserPool", {
+      accountRecovery: AccountRecovery.NONE,
+      autoVerify: { email: true },
+      enableSmsRole: false,
+      removalPolicy: RemovalPolicy.RETAIN,
+      selfSignUpEnabled: false,
+      signInCaseSensitive: false,
+      standardAttributes: {
+        profilePicture: {
+          required: true,
+          mutable: true,
+        },
+        profilePage: {
+          required: true,
+          mutable: true,
+        },
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    });
+    const client = pool.addClient("UserPoolClient", {
+      accessTokenValidity: Duration.minutes(60),
+      authFlows: { adminUserPassword: true },
+      disableOAuth: true,
+      idTokenValidity: Duration.minutes(60),
+      refreshTokenValidity: Duration.days(30),
+      writeAttributes: new ClientAttributes().withStandardAttributes({
+        profilePicture: true,
+        profilePage: true,
+        email: true,
+      }),
+    });
+    new StringParameter(this, "UserPoolClientId", {
+      parameterName: "/spencerduball/env/PUBLIC_USER_POOL_CLIENT_ID",
+      stringValue: client.userPoolClientId,
     });
 
+    // retrieve the cipher info used to encrypt the admin defined passwords
+    const decryptPasswordCipherInfo = new AwsCustomResource(
+      this,
+      "DecryptedPasswordCipherInfo",
+      {
+        onCreate: {
+          service: "SSM",
+          action: "getParameter",
+          parameters: {
+            Name: "/spencerduball/env/PASSWORD_CIPHER_INFO",
+            WithDecryption: true,
+          },
+          physicalResourceId: PhysicalResourceId.fromResponse("Parameter.ARN"),
+        },
+        policy: AwsCustomResourcePolicy.fromSdkCalls({
+          resources: AwsCustomResourcePolicy.ANY_RESOURCE,
+        }),
+      }
+    );
+
+    /////////////////////////////////////////////////////////////////////////////
+    // (2) Github OAuth Sign In
+    //
+    // We need to setup a couple pieces to allow users to authenticate with
+    // Github. First, Github uses OAuth2.0 + PKCE (authorization code) to protect
+    // against CSRF attacks. See this link for full details on the flow:
+    // https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps
+    // Because of this we need to store these authorization codes serverside to
+    // ensure requests are not being intercepted.
+    //
+    // Below we create the table to store the CSRF tokens, as well as create the
+    // routes used for signin and callback.
+    /////////////////////////////////////////////////////////////////////////////
     // get environment variables
     const GITHUB_CLIENT_ID = StringParameter.fromStringParameterName(
       this,
@@ -61,6 +148,14 @@ export class AuthStack extends Stack {
       "env-GITHUB_CLIENT_CALLBACK",
       "/spencerduball/env/GITHUB_CLIENT_CALLBACK"
     );
+
+    // create oauth CSRF validation table
+    const csrfTable = new Table(this, "GithubAuthCSRFTable", {
+      partitionKey: { name: "PK", type: AttributeType.STRING },
+      timeToLiveAttribute: "TTL",
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
     // create the signin route
     const githubSigninFn = new NodejsFunction(this, "GithubSigninFn", {
@@ -95,7 +190,11 @@ export class AuthStack extends Stack {
           GITHUB_CLIENT_SECRET: GITHUB_CLIENT_SECRET.stringValue,
           GITHUB_CLIENT_CALLBACK: GITHUB_CLIENT_CALLBACK.stringValue,
           DDB_TABLE_NAME: csrfTable.tableName,
-          DDB_TABLE_REGION: this.region,
+          REGION: this.region,
+          USER_POOL_ID: pool.userPoolId,
+          USER_POOL_CLIENT_ID: client.userPoolClientId,
+          PASSWORD_CIPHER_INFO:
+            decryptPasswordCipherInfo.getResponseField("Parameter.Value"),
         },
       }
     );
@@ -109,5 +208,6 @@ export class AuthStack extends Stack {
       integration: githubOAuthCallbackIntegration,
     });
     csrfTable.grantReadWriteData(githubOAuthCallbackFn);
+    pool.grant(githubOAuthCallbackFn, "cognito-idp:*");
   }
 }
--- backend/lib/auth/lambda/github-oauth-callback.ts
+++ backend/lib/auth/lambda/github-oauth-callback.ts
@@ -4,14 +4,63 @@ import {
   GetItemCommand,
   DeleteItemCommand,
 } from "@aws-sdk/client-dynamodb";
+import {
+  CognitoIdentityProviderClient,
+  AdminInitiateAuthCommand,
+  AdminCreateUserCommand,
+  AdminSetUserPasswordCommand,
+} from "@aws-sdk/client-cognito-identity-provider";
 import * as Yup from "yup";
 import axios from "axios";
+import crypto from "crypto";
 
 // defined searchParams schema
 const searchParams = Yup.object({
   code: Yup.string().defined(),
   state: Yup.string().defined(),
 });
+
+/**
+ * Encryption utility to encrypt and decrypt messages.
+ *
+ * @param key Should be a 32-length Buffer object.
+ * @param iv Should be a 16-length Buffer object.
+ *
+ * @example const encryptor = new Encryptor(crypto.randomBytes(32), crypto.randomBytes(16));
+ */
+class Encryptor {
+  public readonly algorithm = "aes-256-cbc";
+  private key: Buffer;
+  private iv: Buffer;
+
+  constructor(key: Buffer, iv: Buffer) {
+    this.key = key;
+    this.iv = iv;
+  }
+
+  public encrypt(
+    message: string,
+    inputEnc: crypto.Encoding = "utf-8",
+    outputEnc: crypto.Encoding = "base64"
+  ) {
+    const cipher = crypto.createCipheriv(this.algorithm, this.key, this.iv);
+    return (
+      cipher.update(message, inputEnc, outputEnc) + cipher.final(outputEnc)
+    );
+  }
+
+  public decrypt(
+    encryptedData: string,
+    inputEnc: crypto.Encoding = "base64",
+    outputEnc: crypto.Encoding = "utf-8"
+  ) {
+    const decipher = crypto.createDecipheriv(this.algorithm, this.key, this.iv);
+    return (
+      decipher.update(encryptedData, inputEnc, outputEnc) +
+      decipher.final(outputEnc)
+    );
+  }
+}
 
 export const githubOAuthCallback: APIGatewayProxyHandlerV2<{
   message: string;
@@ -23,7 +72,11 @@ export const githubOAuthCallback: APIGatewayProxyHandlerV2<{
   if (!process.env.GITHUB_CLIENT_SECRET) return { statusCode: 500 };
   if (!process.env.GITHUB_CLIENT_CALLBACK) return { statusCode: 500 };
   if (!process.env.DDB_TABLE_NAME) return { statusCode: 500 };
-  if (!process.env.DDB_TABLE_REGION) return { statusCode: 500 };
+  if (!process.env.REGION) return { statusCode: 500 };
+  if (!process.env.USER_POOL_ID) return { statusCode: 500 };
+  if (!process.env.USER_POOL_CLIENT_ID) return { statusCode: 500 };
+  if (!process.env.PASSWORD_CIPHER_INFO) return { statusCode: 500 };
+  console.log(1);
 
   // get the code and state tokens from Github
   if (!searchParams.isValidSync(event.queryStringParameters)) {
@@ -37,7 +90,7 @@ export const githubOAuthCallback: APIGatewayProxyHandlerV2<{
 
   // ensure a matching state code exists in DDB CSRF table
   const ddb = new DynamoDBClient({
-    region: process.env.DDB_TABLE_REGION,
+    region: process.env.REGION,
     maxAttempts: 3,
   });
   const get = new GetItemCommand({
@@ -57,6 +110,7 @@ export const githubOAuthCallback: APIGatewayProxyHandlerV2<{
       body: JSON.stringify({ message: "Github CSRF code did not match." }),
     };
   }
+  console.log(2);
 
   // get the access token from Github
   const accessToken = await axios.post(
@@ -73,6 +127,7 @@ export const githubOAuthCallback: APIGatewayProxyHandlerV2<{
       },
     }
   );
+  console.log(3);
 
   // get the user info
   const headers = {
@@ -80,9 +135,83 @@ export const githubOAuthCallback: APIGatewayProxyHandlerV2<{
     Accept: "application/vnd.github.v3+json",
   };
   const userName = await axios.get("https://api.github.com/user", { headers });
-  const { login, avatar_url, name } = userName.data;
+  const { login, avatar_url, url, email } = userName.data;
 
-  console.log(login, avatar_url, name);
+  // check if the user exists by attempting to sign in
+  const { key, keyEncoding, iv, ivEncoding } = JSON.parse(
+    process.env.PASSWORD_CIPHER_INFO
+  );
+  const encryptor = new Encryptor(
+    Buffer.from(key, keyEncoding),
+    Buffer.from(iv, ivEncoding)
+  );
+  const cognito = new CognitoIdentityProviderClient({
+    region: process.env.REGION,
+  });
+  console.log(4);
+  const getUser = new AdminInitiateAuthCommand({
+    AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
+    UserPoolId: process.env.USER_POOL_ID,
+    ClientId: process.env.USER_POOL_CLIENT_ID,
+    AuthParameters: {
+      USERNAME: login,
+      PASSWORD: encryptor.encrypt(login),
+    },
+  });
+  try {
+    const res = await cognito.send(getUser);
+    console.log(res);
+  } catch (e) {
+    console.log(JSON.stringify(e));
+
+    // if user doesn't exist, we will create them and sign them in
+    if (e.name === "UserNotFoundException") {
+      // create the new user
+      try {
+        // create user
+        const createUser = new AdminCreateUserCommand({
+          UserPoolId: process.env.USER_POOL_ID,
+          Username: login,
+          UserAttributes: [
+            { Name: "profile", Value: url },
+            { Name: "picture", Value: avatar_url },
+            { Name: "email", Value: email },
+            { Name: "email_verified", Value: "true" },
+          ],
+          MessageAction: "SUPPRESS",
+        });
+        const newUser = await cognito.send(createUser);
+        console.log(newUser);
+
+        // set encrypted password
+        const setPassword = new AdminSetUserPasswordCommand({
+          UserPoolId: process.env.USER_POOL_ID,
+          Username: login,
+          Password: encryptor.encrypt(login),
+          Permanent: true,
+        });
+        const newPassword = await cognito.send(setPassword);
+        console.log(newPassword);
+
+        // sign in user and return tokens
+        const signIn = new AdminInitiateAuthCommand({
+          AuthFlow: "ADMIN_USER_PASSWORD_AUTH",
+          UserPoolId: process.env.USER_POOL_ID,
+          ClientId: process.env.USER_POOL_CLIENT_ID,
+          AuthParameters: {
+            USERNAME: login,
+            PASSWORD: encryptor.encrypt(login),
+          },
+        });
+        const signedIn = await cognito.send(signIn);
+        console.log(signedIn);
+      } catch (e) {
+        console.log("Error here:");
+        console.log(e);
+      }
+    }
+  }
+  console.log(5);
 
   // get the redirect_uri
   const redirect_url = res.Item?.REFERER?.S
