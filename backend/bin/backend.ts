48c1ddedaaec9bf88bfed0ca027fefbd0e8f6fc4
Spencer Duball <spencer@spencerduball.com>
2022-05-29 20:55:06 -0500

    save here to delete stack

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -1,12 +1,48 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
+import { AppStack } from "../lib/app/app-stack";
 import { ApiStack } from "../lib/api/api-stack";
 import { AuthStack } from "../lib/auth/auth-stack";
+import * as Yup from "yup";
 
+//////////////////////////////////////////////////////////////////////////////
+// Check for the required environment variables
+//////////////////////////////////////////////////////////////////////////////
+// ensure the account is defined
+if (!process.env.CDK_DEFAULT_ACCOUNT)
+  throw new Error("CDK_DEFAULT_ACCOUNT is not defined and is required.");
+
+// ensure the NODE_ENV is either "PROD" or "DEV"
+let envSchema = Yup.string().oneOf(["PROD", "DEV"]).required();
+if (envSchema.isValidSync(process.env.NODE_ENV))
+  throw new Error(
+    `NODE_ENV is ${process.env.NODE_ENV} but should be either "PROD" or "DEV"`
+  );
+
+//////////////////////////////////////////////////////////////////////////////
+// Define App Constants
+//////////////////////////////////////////////////////////////////////////////
 // define environments
-const DefaultEnv = { account: "561720044356", region: "us-east-1" } as const;
+const Env = {
+  PROD: { account: "561720044356", region: "us-east-1" },
+  DEV: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-east-1" },
+};
 
-const app = new cdk.App();
-new AuthStack(app, "AuthStack", { env: DefaultEnv });
-new ApiStack(app, "ApiStack", { env: DefaultEnv });
+//////////////////////////////////////////////////////////////////////////////
+// Create the Stacks
+//////////////////////////////////////////////////////////////////////////////
+// create the app
+const app = new App();
+const appStack = new AppStack(app, "AppStack", {
+  env: Env[process.env.NODE_ENV],
+});
+
+// create the api
+new ApiStack(app, "ApiStack", {
+  env: Env[process.env.NODE_ENV],
+  certificate: appStack.certificate,
+});
+
+// create the auth
+new AuthStack(app, "AuthStack", { env: Env[process.env.NODE_ENV] });
--- backend/lib/api/api-stack.ts
+++ backend/lib/api/api-stack.ts
@@ -1,28 +1,56 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { CfnGraphQLApi, CfnGraphQLSchema } from "aws-cdk-lib/aws-appsync";
-import { Asset } from "aws-cdk-lib/aws-s3-assets";
-import path from "path";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import {
+  HttpApi,
+  DomainName,
+  CorsHttpMethod,
+  HttpStage,
+} from "@aws-cdk/aws-apigatewayv2-alpha";
+
+export interface ApiStackProps extends StackProps {
+  certificate: Certificate;
+}
 
 export class ApiStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  public httpApi: HttpApi;
+
+  constructor(scope: Construct, id: string, props?: ApiStackProps) {
     super(scope, id, props);
 
-    // Create the API
-    const api = new CfnGraphQLApi(this, "Api", {
-      authenticationType: "AWS_IAM",
-      name: "Api",
+    // create the domain name
+    const domainName = new DomainName(this, "HttpApiDomainName", {
+      domainName: "api.spencerduball.com",
+      certificate: props.certificate,
     });
 
-    // Upload the schema to S3
-    const apiSchemaFile = new Asset(this, "ApiSchemaFile", {
-      path: path.join(__dirname, "schema.gql"),
+    // create the api
+    this.httpApi = new HttpApi(this, "HttpApi", {
+      apiName: "Api",
+      createDefaultStage: false,
+      corsPreflight: {
+        allowOrigins: ["https://spencerduball.com"],
+        allowMethods: [CorsHttpMethod.ANY],
+      },
     });
 
-    // Create the API schema
-    const schema = new CfnGraphQLSchema(this, "ApiSchema", {
-      apiId: api.attrApiId,
-      definitionS3Location: apiSchemaFile.s3ObjectUrl,
-    });
+    // create stage
+    if (process.env.NODE_ENV === "PROD") {
+      // maps -> https://api.my-domain.com/dev
+      new HttpStage(this, "HttpApiDevStage", {
+        httpApi: this.httpApi,
+        autoDeploy: true,
+        domainMapping: {
+          domainName,
+          mappingKey: "dev",
+        },
+      });
+    } else if (process.env.NOD_ENV === "DEV") {
+      // maps -> https://api.my-domain.com
+      new HttpStage(this, "HttpApiProdStage", {
+        httpApi: this.httpApi,
+        autoDeploy: true,
+      });
+    }
   }
 }
