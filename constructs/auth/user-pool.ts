43ccac323dd10fff3efd024a30abfe4544411b3d
Spencer Duball <spencer@spencerduball.com>
2022-06-12 09:40:11 -0500

    Saving with current user-pool setup

--- constructs/auth/user-pool.ts
+++ constructs/auth/user-pool.ts
@@ -10,10 +10,77 @@ import {
   Script,
   Cron,
 } from "@serverless-stack/resources";
+import { PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 import ms from "ms";
 
 function seconds(milliseconds: number) {
   return milliseconds / 1000;
+}
+
+export interface IKeyRecord {
+  /** The primary key. */
+  PK: `KEY#${string}`;
+  /** The unique ID of the key item. */
+  kid: string;
+  /** The key type. This is used in the jwks.json file to let services know how to verify the key signature. */
+  kty: "RSA";
+  /** The encryption algorithm used. All keys are RS256, RSA + SHA256 hashed. */
+  alg: "RS256";
+  /** Specifies the use of the jwt, in this instance as a signature of the issuer. */
+  use: "sig";
+  /** The exponent for the RSA public key. */
+  e: string;
+  /** The modulus for the RSA public key. */
+  n: string;
+  /** The validity duration in seconds of the refresh token. */
+  refresh_token_duration: number;
+  /** The validity duration in seconds of the access token. */
+  access_token_duration: number;
+  /** The public key in PEM format. */
+  public_key: string;
+  /** The private key in PEM format. */
+  private_key: string;
+  /** The TTL timestamp in seconds. */
+  expires_at?: number;
+}
+
+export interface IUserRecord {
+  /**
+   * The primary key.
+   * @example `USER#${preferred_username}`
+   */
+  PK: `USER#${string}`;
+  /** The type of the user, could be GITHUB, CREDENTIALS, etc. */
+  type: string;
+  /** The username of the user. */
+  preferred_username: string;
+  /** The profile page of the user. */
+  profile: string;
+  /** The profile picture url of the user. */
+  picture: string;
+  /** The email address of the user. */
+  email: string;
+  /** The space delimited scope string of the user. */
+  scope?: string;
+  /** The suspension length for the user. */
+  suspension?: number;
+}
+
+export interface IRefreshTokenRecord {
+  /**
+   * The primary key.
+   * @example `REFRESH_TOKEN#${uuid}`
+   */
+  PK: `REFRESH_TOKEN#${string}`;
+  /** The ID of the refresh token. */
+  uuid: string;
+  /**
+   * The ID of the user associated with the refresh token.
+   * @example `${preferred_username}`
+   */
+  user: string;
+  /** The expiration time of the refresh token in UTC (seconds) format. */
+  expires_at?: number;
 }
 
 export interface UserPoolProps {
@@ -23,14 +90,23 @@ export interface UserPoolProps {
   refreshTokenDuration?: number;
   /** The access token validity duration in seconds. */
   accessTokenDuration?: number;
+  /**
+   * The key rotation frequency.
+   *
+   * IMPORTANT! Make sure if `rotationFrequency` is specified that the frequency is LESS
+   * than the `refreshTokenDuration` or else your keyTable will grow infinately!
+   * */
+  rotationFrequency?: `rate(${string})`;
 }
 
 export class UserPool extends Construct {
   public readonly refreshTokenDuration: number;
   public readonly accessTokenDuration: number;
+  public readonly rotationFrequency: `rate(${string})`;
   public readonly publicBucket: Bucket;
   public readonly keyTable: Table;
-  public readonly otcTable: Table;
+  public readonly userTable: Table;
+  public readonly invokeApiStatement: PolicyStatement;
 
   constructor(scope: Stack, id: string, props: UserPoolProps) {
     super(scope, id);
@@ -40,6 +116,17 @@ export class UserPool extends Construct {
       props.refreshTokenDuration || seconds(ms("7 days"));
     this.accessTokenDuration =
       props.accessTokenDuration || seconds(ms("1 hour"));
+
+    // set the rotation frequency
+    this.rotationFrequency = props.rotationFrequency || "rate(90 days)";
+    if (this.refreshTokenDuration >= seconds(ms(this.rotationFrequency))) {
+      if (props.rotationFrequency) {
+        const errorMsg = `rotationFrequency = ${props.rotationFrequency} must be greater than refreshTokenDuration = ${props.refreshTokenDuration}`;
+        throw new Error(errorMsg);
+      }
+      let newRate = ms(this.refreshTokenDuration * 2, { long: true });
+      this.rotationFrequency = `rate(${newRate})`;
+    }
 
     ////////////////////////////////////////////////////////////////////////
     // (1) Setup Infrastructure
@@ -57,7 +144,8 @@ export class UserPool extends Construct {
       },
     });
 
-    // create a table to store the RSA key info
+    // create a table to store the RSA key info.
+    /** {@link IKeyRecord} */
     this.keyTable = new Table(scope, "KeyTable", {
       fields: { PK: "string" },
       primaryIndex: { partitionKey: "PK" },
@@ -70,11 +158,28 @@ export class UserPool extends Construct {
       },
     });
 
-    // create a table to store the auth one-time-codes
-    this.otcTable = new Table(scope, "OTCTable", {
+    // create the user table
+    /** {@link IUserRecord} */
+    /** {@link IRefreshTokenRecord} */
+    this.userTable = new Table(scope, "UserTable", {
       fields: { PK: "string" },
       primaryIndex: { partitionKey: "PK" },
-      cdk: { table: { removalPolicy: RemovalPolicy.DESTROY } },
+      cdk: {
+        table: {
+          removalPolicy: RemovalPolicy.DESTROY,
+          timeToLiveAttribute: "expires_at",
+        },
+      },
+    });
+
+    // expose invoke permissions statement for user pool api. Other providers can grant their
+    // functions this permissions statement to get tokens, CRUD users
+    this.invokeApiStatement = new PolicyStatement({
+      actions: ["execute-api:Invoke"],
+      effect: Effect.ALLOW,
+      resources: [
+        `arn:aws:execute-api:${scope.region}:${scope.account}:${props.api.httpApiId}/*/*/*`,
+      ],
     });
 
     ////////////////////////////////////////////////////////////////////////
@@ -111,9 +216,9 @@ export class UserPool extends Construct {
     // create keys on initial deployment
     new Script(scope, "CreateInitialKeys", { onCreate: rotateConfig });
 
-    // setup cron job that rotates keys every 3 months
+    // setup cron job that rotates keys
     new Cron(scope, "ScheduledKeyRotation", {
-      schedule: "rate(90 days)",
+      schedule: this.rotationFrequency,
       job: rotateFn,
     });
 
@@ -145,5 +250,41 @@ export class UserPool extends Construct {
       },
     });
     ///////////////////////////////////////////////////////////
+
+    // setup tokening service routes
+    props.api.addRoutes(scope, {
+      "GET /token": {
+        function: {
+          handler: "functions/auth/token.handler",
+          permissions: [this.keyTable, this.userTable],
+          environment: {
+            KEY_TABLE: this.keyTable.tableName,
+            USER_TABLE: this.userTable.tableName,
+          },
+        },
+        authorizer: "iam",
+      },
+    });
+    // CRUD User
+    // POST /user - Need to be able to create users from providers.
+    // GET /user/{id} - Need to be able to get user info.
+    // DELETE /user/{id} - Need to be able to delete users from pool.
+    // PATCH /user/{id} - Need to be able to update user info.
+
+    // seed the database for test
+    if (scope.stage !== "prod") {
+      new Function(scope, "SeedUserTableFn", {
+        handler: "functions/auth/user-pool/seed-user-table.handler",
+        environment: { USER_TABLE: this.userTable.tableName },
+        permissions: [this.userTable],
+      });
+      new Script(scope, "SeedUserTable", {
+        onCreate: {
+          handler: "functions/auth/user-pool/seed-user-table.handler",
+          environment: { USER_TABLE: this.userTable.tableName },
+          permissions: [this.userTable],
+        },
+      });
+    }
   }
 }
