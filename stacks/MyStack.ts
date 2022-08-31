65c482646e754dfeffaaaee205a34221bd8151b7
Spencer Duball <spencer@spencerduball.com>
2022-08-31 11:19:34 -0500

    Added remix.run and chakra styling

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,31 +1,13 @@
-import {
-  Table,
-  RemixSite,
-  StackContext,
-} from "@serverless-stack/resources";
+import { RemixSite, StackContext } from "@serverless-stack/resources";
 
 export function MyStack({ stack }: StackContext) {
-
-  // Create a DynamoDB table
-  const table = new Table(stack, "Counter", {
-    fields: {
-      counter: "string",
-    },
-    primaryIndex: { partitionKey: "counter" },
-  });
-
   // Create a Remix site
   const site = new RemixSite(stack, "web", {
     path: "web/",
-    environment: {
-      REGION: stack.region,
-      TABLE_NAME: table.tableName,
-    }
-  })
-  site.attachPermissions([table]);
+    environment: { REGION: stack.region },
+  });
 
   stack.addOutputs({
     SiteURL: site.url,
-    TableName: table.tableName,
   });
 }
--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,34 +1,9 @@
-import type { ActionFunction, LoaderFunction, LinksFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
-
-import { getCount, incrementCount } from "~/models/counter.server";
-import stylesUrl from "~/styles/index.css";
-
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: stylesUrl }];
-};
-
-export const loader: LoaderFunction = async() => {
-  return json({
-    count: await getCount()
-  });
-}
-
-export const action: ActionFunction = async() => {
-  return json({
-    count: await incrementCount()
-  });
-}
+import { Box, Text } from "@chakra-ui/react";
 
 export default function Index() {
-  const { count } = useLoaderData();
   return (
-    <div className="App">
-      <p>You clicked me {count} times.</p>
-      <Form method="post">
-        <button type="submit">Click Me!</button>
-      </Form>
-    </div>
+    <Box bg="blue.9">
+      <Text color="red.9">Hello There Now</Text>
+    </Box>
   );
 }
