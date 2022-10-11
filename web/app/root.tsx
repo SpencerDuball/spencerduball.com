9b674b3c80cedc239bb5053cbba903d23d5425b6
Spencer Duball <spencer@spencerduball.com>
2022-10-11 17:08:13 -0500

    Going to localStorageManager for the theme until a fix is rolled out for https://github.com/chakra-ui/chakra-ui/issues/6192

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -1,18 +1,9 @@
 // root.tsx
 import React, { useContext, useEffect } from "react";
 import { withEmotionCache } from "@emotion/react";
-import { ChakraProvider, cookieStorageManagerSSR, localStorageManager, useColorMode } from "@chakra-ui/react";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useFetcher,
-  useLoaderData,
-} from "@remix-run/react";
-import type { MetaFunction, LoaderFunction } from "@remix-run/node"; // Depends on the runtime you choose
+import { ChakraProvider, localStorageManager } from "@chakra-ui/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "@remix-run/react";
+import type { MetaFunction } from "@remix-run/node"; // Depends on the runtime you choose
 import { theme } from "@dub-stack/chakra-radix-colors";
 import { Grid, Container, extendTheme } from "@chakra-ui/react";
 import { Header } from "~/components";
@@ -69,34 +60,14 @@ const Document = withEmotionCache(({ children }: DocumentProps, emotionCache) =>
 
 const customTheme = extendTheme({
   ...theme,
+  config: {
+    initialColorMode: "dark",
+  },
   fonts: {
     heading: "Inter, sans-serif",
     body: "Inter, sans-serif",
   },
 });
-
-export const loader: LoaderFunction = async ({ request }) => {
-  console.log("SERVER: ", request.headers.get("cookie"));
-  const cookie = request.headers.get("cookie");
-  if (cookie) return cookie;
-  else return "";
-};
-
-const Yo = () => {
-  const { colorMode } = useColorMode();
-  const fetcher = useFetcher();
-
-  useEffect(() => {
-    console.log(document.cookie);
-  }, [colorMode]);
-
-  // print out the client info
-  console.log(`Client_Side: colorMode=${colorMode}`);
-  if (typeof window !== "undefined") console.log(`Client_Side: cookie=${document.cookie}`);
-  console.log("\n");
-
-  return null;
-};
 
 export default function App() {
   const cookies = useLoaderData();
@@ -104,12 +75,8 @@ export default function App() {
 
   return (
     <Document>
-      <ChakraProvider
-        theme={customTheme}
-        colorModeManager={typeof cookies === "string" ? cookieStorageManagerSSR(cookies) : localStorageManager}
-      >
+      <ChakraProvider theme={customTheme} colorModeManager={localStorageManager}>
         <Grid gap={8}>
-          <Yo />
           <Header />
           <Grid as="main">
             <Container maxW="container.lg">
