090ed586bb652e55816ccf137659c29b7093c424
Spencer Duball <spencer@spencerduball.com>
2022-11-07 17:05:19 -0600

    Added settings restore

--- web/app/components/mdx-editor/mdx-editor.tsx
+++ web/app/components/mdx-editor/mdx-editor.tsx
@@ -2,9 +2,37 @@ import { useEffect, useRef, useState } from "react";
 import { Box } from "@chakra-ui/react";
 import type { BoxProps } from "@chakra-ui/react";
 import { Toolbar } from "./toolbar";
-import { useMdxEditorStore } from "./context";
+import { useMdxEditorState, useMdxEditorStore } from "./context";
 import { useWindowSize } from "react-use";
-import { MdxView } from "./mdx-view/mdx-view";
+import { MdxView } from "./mdx-view";
+import { z } from "zod";
+
+// EditorSettings - constants and type safety
+const MdxEditorSettingsKey = "mdx-editor-settings";
+const ZMdxEditorSettings = z.object({
+  isVimMode: z.boolean(),
+  theme: z.enum(["system", "light", "dark"]),
+  view: z.enum(["code", "preview", "attachments"]),
+});
+
+/** Saves and restores the editor settings to/from localStorage. */
+const useRestoreSettings = () => {
+  let state = useMdxEditorState();
+  const store = useMdxEditorStore();
+
+  // restore settings on initial render
+  useEffect(() => {
+    const storageSettingsString = localStorage.getItem(MdxEditorSettingsKey);
+    if (storageSettingsString) {
+      const storageSettings = ZMdxEditorSettings.safeParse(JSON.parse(storageSettingsString));
+      if (storageSettings.success) state.settings = { ...storageSettings.data, view: "code" };
+      else localStorage.setItem(MdxEditorSettingsKey, JSON.stringify(state.settings));
+    }
+  }, []);
+
+  // update localStorage on settings update
+  useEffect(() => localStorage.setItem(MdxEditorSettingsKey, JSON.stringify(store.settings)), [store.settings]);
+};
 
 /** Computes the non-dynamic height and width for the codemirror editor. */
 const useCodeMirrorDimensions = () => {
@@ -34,8 +62,13 @@ const useCodeMirrorDimensions = () => {
 export interface MdxEditorProps extends BoxProps {}
 
 export const MdxEditor = (props: MdxEditorProps) => {
-  const { height, width, containerRef, toolbarRef } = useCodeMirrorDimensions();
   const store = useMdxEditorStore();
+
+  // persist the editor settings
+  useRestoreSettings();
+
+  // compute the codemirror dimensions
+  const { height, width, containerRef, toolbarRef } = useCodeMirrorDimensions();
 
   return (
     <Box ref={containerRef} display="grid" gridTemplateRows="max-content 1fr" {...props}>
