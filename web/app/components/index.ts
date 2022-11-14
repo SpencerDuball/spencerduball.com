a4b2ad25a5ec80f7413cf726ee70ddd24e87fa2f
Spencer Duball <spencer@spencerduball.com>
2022-11-14 08:11:47 -0600

    Started on WelcomeCard for dashboard page

--- web/app/components/index.ts
+++ web/app/components/index.ts
@@ -3,3 +3,4 @@ export * from "./icon";
 export * from "./profile-card";
 export * from "./mdx-editor";
 export * from "./scroll-box";
+export * from "./stat";
--- web/app/routes/dashboard/index.tsx
+++ web/app/routes/dashboard/index.tsx
@@ -1,6 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Link, Grid, Flex, Text, Button } from "@chakra-ui/react";
+import type { GridProps } from "@chakra-ui/react";
 import { getUser } from "~/session.server";
-import { LoaderArgs, redirect } from "@remix-run/node";
+import { json, LoaderArgs, redirect } from "@remix-run/node";
+import { useThemedColor } from "@dub-stack/chakra-radix-colors";
+import { Stat } from "~/components";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const user = await getUser(request, "required");
@@ -8,9 +11,47 @@ export const loader = async ({ request }: LoaderArgs) => {
   // ensure user is admin
   if (!user.roles?.includes("admin")) throw redirect("/");
 
-  return null;
+  return json({ monthlyViews: 75 });
 };
 
+// WelcomeCard
+////////////////////////////////////////////////////////////////////////////////
+interface WelcomeCardProps extends GridProps {}
+
+const WelcomeCard = (props: WelcomeCardProps) => {
+  const c = useThemedColor();
+
+  return (
+    <Grid
+      templateColumns="1fr max-content"
+      p={8}
+      borderRadius="lg"
+      boxShadow="md"
+      border="1px solid"
+      borderColor={c("_gray.6")}
+      bg={c("_gray.2")}
+      {...props}
+    >
+      <Flex flexDir="column" gap={2}>
+        <Text fontSize="4xl">Welcome back, Spencer!</Text>
+        <Flex gap={4} alignItems="center">
+          <Button w="32" colorScheme="red">
+            Logout
+          </Button>
+          <Link>View Analytics</Link>
+        </Flex>
+      </Flex>
+      <Stat w="xs" bg={c("_gray.4")} label="Monthly Views" value="75" />
+    </Grid>
+  );
+};
+
+// Dashboard
+////////////////////////////////////////////////////////////////////////////////
 export default function Dashboard() {
-  return <Box>Hello!</Box>;
+  return (
+    <Box>
+      <WelcomeCard />
+    </Box>
+  );
 }
