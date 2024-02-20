25e9d686e9c347407029e776493d4e43b5cd9c02
Spencer Duball <spencer@spencerduball.com>
2024-02-19 20:27:09 -0600

    Working on file-upload-box, need to fix the syntax highlighting again. Fuck this shit, breaks every 5 minutes.

--- packages/web/app/lib/ui/file-upload-box/index.tsx
+++ packages/web/app/lib/ui/file-upload-box/index.tsx
@@ -7,12 +7,24 @@ export interface FileUploadBoxProps extends React.ComponentProps<"div"> {}
 export function FileUploadBox({ className, ...props }: FileUploadBoxProps) {
   const inputRef = React.useRef<HTMLInputElement>(null!);
 
+  // handle drag and drop events
+  function handleDragOverEvent(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+  }
+  function handleDropEvent(e: React.DragEvent<HTMLDivElement>) {
+    e.preventDefault();
+    const files = e.dataTransfer.files;
+    console.log({ files });
+  }
+
   return (
     <div
       className={cn(
         "grid cursor-pointer place-items-center overflow-hidden rounded-lg border-4 border-dashed border-slateA-4 text-slateA-6",
         className,
       )}
+      onDragOver={handleDragOverEvent}
+      onDrop={handleDropEvent}
       {...props}
     >
       <input ref={inputRef} className="hidden" type="file" />
