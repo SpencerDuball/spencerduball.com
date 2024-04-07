3efbb2545f59c1a43e9b8d357145f9d4f638be18
Spencer Duball <spencer@spencerduball.com>
2024-04-07 10:28:16 -0500

    Fresh start, rebuilding with fly.io

--- packages/web/app/routes/dashboard.cms._index.tsx
+++
@@ -1,6 +0,0 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  return redirect("/dashboard/cms/blog");
-}
