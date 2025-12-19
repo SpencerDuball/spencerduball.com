44d20a7ecf7e0130f3974c1d2a1986b5c5a3e6d8
Spencer Duball <spencer@spencerduball.com>
2025-12-19 16:19:23 -0600

    fix: added fix to remove nitro and enable ssg

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,13 +3,11 @@ import { devtools } from "@tanstack/devtools-vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 import { fileURLToPath } from "node:url";
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    nitro(),
     tailwindcss(),
     tanstackStart({
       prerender: {
